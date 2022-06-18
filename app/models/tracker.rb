5bf668325f943a740c2643ec1e325efd53ff6f29
vitaly <vitalij.desuk@gmail.com>
2022-06-18 15:54:17 +0300

    remove google dustance service

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -20,58 +20,6 @@ class Tracker < ApplicationRecord
     }
   end
 
-  # GQL createUpdateOdometer
-  # If Distance API pass and Odometer is created, then should leave boolean flag true on a records.
-  # def create_odometer
-  #   # Get last 4 Trackers and check that none been called to Distance API
-  #   trackers = Tracker.where(vehicle_id: self.vehicle_id).order("created_at ASC").last(4).map{|tracker| tracker unless tracker.distance_api}.compact
-  #
-  #   if trackers.count == 4
-  #     distance = request_distance_api(trackers)
-  #     distance.build_response
-  #     Rollbar.log("error", "#{distance.errors}") if distance.errors.present?
-  #
-  #     if distance.result.present?
-  #       odometer = request_odometer(odometer_params(distance.result))
-  #       odometer.build_response
-  #       Rollbar.log("error", "Could not create an odometer: #{odometer.errors}") if odometer.errors.present?
-  #
-  #       trackers.each{ |tracker| tracker.update(distance_api: true) } if odometer.result.present?
-  #     end
-  #   end
-  # end
-
-  # def request_odometer(params)
-  #   Vehicles::CreateUpdateOdometer.new(params)
-  # end
-  #
-  # def request_distance_api(trackers)
-  #   GoogleMaps::Distance.new(origins_coordinates(trackers), destinations_coordinates(trackers))
-  # end
-
-  # createUpdateOdometer payload
-  # def odometer_params(params)
-  #   {
-  #     vehicle_id: self.vehicle_id,
-  #     origin_address: params["origin_addresses"].first,
-  #     destination_address: params["destination_addresses"].first,
-  #     duration: params["rows"][0]["elements"][0]["duration"]["value"],
-  #     distance: params["rows"][0]["elements"][0]["distance"]["value"],
-  #     city: current_tracker.city,
-  #     within_radius: current_tracker.within_radius
-  #   }
-  # end
-
-  # Distance API payload
-  # def origins_coordinates(trackers)
-  #   { latitude: trackers.first[:latitude], longitude: trackers.first[:longitude] }
-  # end
-  #
-  # # Distance API payload
-  # def destinations_coordinates(trackers)
-  #   { latitude: trackers.last[:latitude], longitude: trackers.last[:longitude] }
-  # end
-
   def current_tracker
     Tracker.find(self.id)
   end
--- db/migrate/20220504123850_create_tracker.rb
+++ db/migrate/20220504123850_create_tracker.rb
@@ -5,11 +5,6 @@ class CreateTracker < ActiveRecord::Migration[6.1]
       t.float :longitude
       t.string :speed
       t.uuid :vehicle_id
-      t.boolean :within_radius
-      t.string :city
-      t.integer :radius_size
-      t.float :radius_longitude
-      t.float :radius_latitude
 
       t.timestamps
     end
