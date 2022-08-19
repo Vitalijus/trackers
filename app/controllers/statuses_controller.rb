d59f0eb7c445314dbbc708762c60b0ec96100a8d
vitaly <vitalij.desuk@gmail.com>
2022-08-19 13:16:53 +0300

    update database.yml file

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -2,7 +2,8 @@ class StatusesController < ApplicationController
   def show
     render json: {
       status: "OK",
-      ruby_version: Rails::VERSION::STRING,
+      rails_version: Rails::VERSION::STRING,
+      ruby_version: RUBY_VERSION,
       rails_env: Rails.env,
       config: ActiveRecord::Base.connection_config.slice(:database, :host, :port)
     }
