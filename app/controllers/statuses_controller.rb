3581d23ec7f07d5c5d669cfdb3113b1357372ac9
vitaly <vitalij.desuk@gmail.com>
2022-08-22 16:16:25 +0300

    update controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -6,6 +6,7 @@ class StatusesController < ApplicationController
       ruby_version: RUBY_VERSION,
       rails_env: Rails.env,
       config: ActiveRecord::Base.connection_config.slice(:database, :host, :port)
+      # tcp_open: Socket.tcp("www.ruby-lang.org", 10567, connect_timeout: 5) {}
     }
   end
 end
